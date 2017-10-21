2e4f9df4cd04fb2b8606f80d8158f0ac18518b3b
Karoline Bonnerud <kbonnerud@gmail.com>
2017-10-21 16:04:33 +0200

    react js tutorial 3 done

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,7 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var App = require('./components/App');
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+import Layout from "./components/Layout";
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
